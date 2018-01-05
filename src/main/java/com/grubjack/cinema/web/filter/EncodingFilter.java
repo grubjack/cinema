7c2a7ab76e342c7e5d909fdb35dafc3e90e3fe61
grubjack <grubjack@gmail.com>
2018-01-06 01:11:13 +0200

    add UTF-8,Locale web filters

---
+++ src/main/java/com/grubjack/cinema/web/filter/EncodingFilter.java
@@ -0,0 +1,24 @@
+package com.grubjack.cinema.web.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by Urban Aleksandr
+ */
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
