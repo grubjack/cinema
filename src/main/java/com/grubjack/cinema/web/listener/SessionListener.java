8d764bdafb27c327ca1c21865ab998ccb5861b13
grubjack <grubjack@gmail.com>
2018-01-06 01:13:00 +0200

    impl Session Listener

---
+++ src/main/java/com/grubjack/cinema/web/listener/SessionListener.java
@@ -0,0 +1,35 @@
+package com.grubjack.cinema.web.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Created by Urban Aleksandr
+ */
+public class SessionListener implements HttpSessionListener {
+
+    private static Logger log = LoggerFactory.getLogger(SessionListener.class);
+    private int sessionCount = 0;
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        synchronized (this) {
+            sessionCount++;
+        }
+        log.info("Session with id {} is created", se.getSession().getId());
+        log.info("Total Sessions: {}", sessionCount);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        synchronized (this) {
+            sessionCount--;
+        }
+        log.info("Session with id {} is destroyed", se.getSession().getId());
+        log.info("Total Sessions: {}", sessionCount);
+    }
+
+}
